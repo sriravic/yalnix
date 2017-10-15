cea6d423ecb81d4197a9ec1a4107e7721db914f3
sriravic <srinath.pop@gmail.com>
2017-10-15 12:59:17 -0400

    added logic for initial ptes

--- yalnix/src/kernel.c
+++ yalnix/src/kernel.c
@@ -6,6 +6,12 @@
 
 void* globalBrk;
 
+typedef struct pte PageTableEntry;
+unsigned int gNumPagesR0;		// region0 pages
+unsigned int gNumPagesR1;		// region1 pages
+
+PageTableEntry* gPageTable;
+
 unsigned int getKB(unsigned int size) { return size / (1 << 10); }
 unsigned int getMB(unsigned int size) { return size / (1 << 20); }
 unsigned int getGB(unsigned int size) { return size / (1 << 30); }
@@ -23,9 +29,40 @@ void SetKernelData(void* _KernelDataStart, void* _KernelDataEnd)
     printf("DataStart  : 0x%08X\n", _KernelDataStart);
     printf("DataEnd    : 0x%08X\n", _KernelDataEnd);
 
-    unsigned int startAddress = 0;
+	// create the initial page tables
+	unsigned int dataEnd = (unsigned int)_KernelDataEnd;
+	unsigned int dataEndRounded = UP_TO_PAGE(dataEnd);
+	printf("Data End Rounded : 0x%08X\n", dataEndRounded);
+	
+	unsigned int NUM_PAGES = dataEndRounded / PAGE_SIZE;
+	printf("Num physical pages : %u\n", NUM_PAGES);
+	
+	gPageTable = (PageTableEntry*)malloc(sizeof(PageTableEntry) * NUM_PAGES);
+	if(gPageTable != NULL)
+	{
+		gNumPagesR0 = NUM_PAGES;
+		for(int i = 0; i < NUM_PAGES; i++)
+		{
+			gPageTable[i].valid = 1;
+			gPageTable[i].prot = PROT_READ | PROT_EXEC;
+			gPageTable[i].pfn = i;
+		}
+	}
+	else
+	{
+		printf("error creating initial page tables\n");		
+	}
 }
 
+
+void DoIdle(void)
+{
+	while(1)
+	{
+		TracePrintf(1, "DoIdle\n");
+		Pause();
+	}
+}
 
 void KernelStart(char** argv, unsigned int pmem_size, UserContext* uctx)
 {
