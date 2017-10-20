9bd2fc3cee03e0805c9820cc1cc0fb5716a66a80
sriravic <srinath.pop@gmail.com>
2017-10-20 12:03:19 -0400

    still bugs there

--- yalnix/src/kernel.c
+++ yalnix/src/kernel.c
@@ -106,16 +106,27 @@ void KernelStart(char** argv, unsigned int pmem_size, UserContext* uctx)
 	unsigned int dataStartRounded = UP_TO_PAGE(dataStart);
 	unsigned int dataEndRounded = UP_TO_PAGE(dataEnd);
 	unsigned int textEnd = DOWN_TO_PAGE(dataStart);
-	unsigned int NUM_TEXT_FRAMES_IN_USE = textEnd / PAGESIZE;
-	unsigned int NUM_DATA_FRAMES_IN_USE = (dataEndRounded - dataStartRounded) / PAGESIZE;
+	unsigned int TEXT_FRAME_END_PAGENUM = textEnd / PAGESIZE;
+	unsigned int DATA_FRAME_END_PAGENUM = dataEnd / PAGESIZE;
+	unsigned int NUM_DATA_FRAMES_IN_USE = DATA_FRAME_END_PAGENUM - TEXT_FRAME_END_PAGENUM;
+	unsigned int NUM_FRAMES_IN_USE = DATA_FRAME_END_PAGENUM;
+
+	TracePrintf(0, "Data End Address : 0x%08x\n", dataEndRounded);
+	TracePrintf(0, "Global brk : 0x%08x\n",UP_TO_PAGE((unsigned int)globalBrk));
+	TracePrintf(0, "Text Frame End : %u\n", TEXT_FRAME_END_PAGENUM);
+	TracePrintf(0, "Data Frame End : %u\n", DATA_FRAME_END_PAGENUM);
+
+	TracePrintf(0, "Total Physical Frames : %u\n", TOTAL_FRAMES);
+	TracePrintf(0, "Total Frames In USE : %u\n", NUM_FRAMES_IN_USE);
+	TracePrintf(0, "Total Remaining pages : %u\n", TOTAL_FRAMES - NUM_FRAMES_IN_USE);
 
 	// first initialize the pools
 	gUsedFramePool.m_head = 1; gFreeFramePool.m_head = 1;
 	
 	// allocate the first required 'N' frames in allocated
 	int frameNum;
-	FrameTableEntry curr = gUsedFramePool;
-	for(frameNum = 0; frameNum < NUM_TEXT_FRAMES_IN_USE + NUM_DATA_FRAMES_IN_USE; frameNum++)
+	FrameTableEntry* curr = &gUsedFramePool;
+	for(frameNum = 0; frameNum < NUM_FRAMES_IN_USE; frameNum++)
 	{
 		FrameTableEntry* next = (FrameTableEntry*)malloc(sizeof(FrameTableEntry));
 		if(next != NULL)
@@ -123,8 +134,8 @@ void KernelStart(char** argv, unsigned int pmem_size, UserContext* uctx)
 			next->m_frameNumber = frameNum;
 			next->m_head = 0;
 			next->m_next = NULL;
-			curr.m_next = next;
-			curr = *next;
+			curr->m_next = next;
+			curr = next;
 		}
 		else
 		{
@@ -133,8 +144,8 @@ void KernelStart(char** argv, unsigned int pmem_size, UserContext* uctx)
 		}
 	}
 
-	curr = gFreeFramePool;
-	for(frameNum = NUM_TEXT_FRAMES_IN_USE + NUM_DATA_FRAMES_IN_USE; frameNum < TOTAL_FRAMES; frameNum++)
+	curr = &gFreeFramePool;
+	for(frameNum = NUM_FRAMES_IN_USE; frameNum < TOTAL_FRAMES; frameNum++)
 	{
 		FrameTableEntry* next = (FrameTableEntry*)malloc(sizeof(FrameTableEntry));
 		if(next != NULL)
@@ -142,8 +153,8 @@ void KernelStart(char** argv, unsigned int pmem_size, UserContext* uctx)
 			next->m_frameNumber = frameNum;
 			next->m_head = 0;
 			next->m_next = NULL;
-			curr.m_next = next;
-			curr = *next;
+			curr->m_next = next;
+			curr = next;
 		}
 		else
 		{
@@ -152,15 +163,19 @@ void KernelStart(char** argv, unsigned int pmem_size, UserContext* uctx)
 		}
 	}
 
+	// update the heap allocations if any
+	unsigned int NUM_HEAP_FRAMES_IN_USE = (UP_TO_PAGE((unsigned int)globalBrk) - dataEndRounded) / PAGESIZE;
+	TracePrintf(0, "Total Heap Frames : %u\n", NUM_HEAP_FRAMES_IN_USE);
+
 	// Initialize the page tables for the kernel
 	// map the used pages to used frames as one-one mapping
-	for(i = 0; i < NUM_TEXT_FRAMES_IN_USE; i++)
+	for(i = 0; i < TEXT_FRAME_END_PAGENUM; i++)
 	{
 		gKernelPageTable[i].valid = 1;
 		gKernelPageTable[i].prot = PROT_READ|PROT_EXEC;
 		gKernelPageTable[i].pfn = i;
 	}
-	for(i = NUM_TEXT_FRAMES_IN_USE; i < NUM_TEXT_FRAMES_IN_USE + NUM_DATA_FRAMES_IN_USE; i++)
+	for(i = TEXT_FRAME_END_PAGENUM; i < NUM_FRAMES_IN_USE; i++)
 	{
 		gKernelPageTable[i].valid = 1;
 		gKernelPageTable[i].prot = PROT_READ|PROT_WRITE;
@@ -169,36 +184,47 @@ void KernelStart(char** argv, unsigned int pmem_size, UserContext* uctx)
 
 	// Allocate one page for kernel stack region
 	// This has to be in the same spot
-	unsigned int stackIndex = KERNEL_STACK_BASE / PAGESIZE;
+	unsigned int stackIndex = (KERNEL_STACK_BASE / PAGESIZE);
+	TracePrintf(0, "Kernel stack index : %u\n", stackIndex);
 
-	// find the first free frame in the available pool
-	// move it to the end of the used pool list
-	// and assign that frame to this kernel stack page
-	FrameTableEntry* avail = gFreeFramePool.m_next;
-	if(avail != NULL)
+	// Find two frames that are in the free list at STACK_BASE and STACK_BASE - 1
+	// and move them to used list. and allocate the pte entries to these frames
+	// we need this because VM is not enabled yet and hence we have to map one-one
+	FrameTableEntry* curr = &gFreeFramePool;
+	FrameTableEntry* next = curr->m_next;
+	while(curr != NULL)
 	{
-		// set the head of the avail to point to the correct node now
-		gFreeFramePool.m_next = avail->m_next;
+		if(next->m_frameNumber == stackIndex)
+		{
+			// remove this frame and the next frame and move it to the other used pool
+			FrameTableEntry* f1 = next;
+			FrameTableEntry* f2 = next->m_next;
 
-		// iterate through the used list to paste the avail
-		FrameTableEntry* curr = &gUsedFramePool;
-		FrameTableEntry* next = curr->m_next;
-		while(next != NULL)
+			TracePrintf(0, "Stack Frame pfn : %u\n", f1->m_frameNumber);
+			TracePrintf(0, "Stack frame pfn : %u\n", f2->m_frameNumber);
+
+			// reassign the pointers
+			curr->m_next = f2->m_next;
+
+			// move to the end of the free list
+
+			// set ptes
+			gKernelPageTable[stackIndex].valid = 1;
+			gKernelPageTable[stackIndex].prot = PROT_READ|PROT_WRITE;
+			gKernelPageTable[stackIndex].pfn = f1->m_frameNumber;
+			gKernelPageTable[stackIndex - 1].valid = 1;
+			gKernelPageTable[stackIndex - 1].prot = PROT_READ|PROT_WRITE;
+			gKernelPageTable[stackIndex - 1].pfn = f2->m_frameNumber;
+		}
+		else
 		{
 			curr = next;
 			next = next->m_next;
 		}
-
-		// insert the node
-		// and reset the pointers
-		curr->m_next = avail;
-		avail->m_next = NULL;
-
-		// assign this frame number to the pagetable entry
-		gKernelPageTable[stackIndex].valid = 1;
-		gKernelPageTable[stackIndex].prot = PROT_READ | PROT_WRITE;
-		gKernelPageTable[stackIndex].pfn = avail->m_frameNumber;
 	}
+	
+
+
 	
 	// set the page table addresses in the registers
 	WriteRegister(REG_PTBR0, (unsigned int)gKernelPageTable);
@@ -208,6 +234,7 @@ void KernelStart(char** argv, unsigned int pmem_size, UserContext* uctx)
 	
 	// enable virtual memory
 	WriteRegister(REG_VM_ENABLE, 1);
+	gVMemEnabled = 1;
 
 	// run idle proces
 	// on the top of the VM region - very hacky approach
