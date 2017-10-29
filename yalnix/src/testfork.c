c374197cafa64d29432afea1907088f31a394abf
sriravic <srinath.pop@gmail.com>
2017-10-29 19:01:45 -0400

    added new files

---
+++ yalnix/src/testfork.c
@@ -0,0 +1,27 @@
+#include <yalnix.h>
+
+int main(int argc, char** argv)
+{
+    int rc = Fork();
+    if(rc == 0)
+    {
+        TracePrintf(0, "fork succeeded : Child process\n");
+        int cpid = GetPid();
+        while(1)
+        {
+            TracePrintf(0, "Child Process : %d\n", cpid);
+            Pause();
+        }
+    }
+    else
+    {
+        TracePrintf(0, "fork succeeded : parent process\n");
+        int ppid = GetPid();
+        while(1)
+        {
+            TracePrintf(0, "Parent Process : %d\n", ppid);
+            Pause();
+        }
+    }
+    return 0;
+}
