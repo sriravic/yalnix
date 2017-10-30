267d0c19b14c3eb846c78206095024a63e7c7055
sriravic <srinath.pop@gmail.com>
2017-10-29 20:45:41 -0400

    added code to copy kernel context also

--- yalnix/src/interrupt_handler.c
+++ yalnix/src/interrupt_handler.c
@@ -164,7 +164,6 @@ void interruptClock(UserContext* ctx)
 
 			// update the user context
 			currPCB->m_ticks = 0;	// reset ticks
-
 			PCB* nextPCB = getHeadProcess(&gReadyToRunProcessQ);
 
 			int rc = KernelContextSwitch(MyKCS, currPCB, nextPCB);
