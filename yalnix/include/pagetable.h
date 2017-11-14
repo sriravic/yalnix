16272c418bd0ae86691b95fe533ceaaf09cff66c
sriravic <srinath.pop@gmail.com>
2017-11-14 11:43:09 -0500

    adding new pagetable definitions

--- yalnix/include/pagetable.h
+++ yalnix/include/pagetable.h
@@ -10,6 +10,27 @@ struct pagetable
 	PageTableEntry m_pte[NUM_VPN];
 };
 
+// some global constants we precompute for ease of DiskAccess
+const unsigned int gKStackPages = unsigned int(KERNEL_STACK_MAXSIZE / PAGESIZE);
+const unsigned int gR0Pages 	= unsigned int(VMEM_0_SIZE 			/ PAGESIZE) - gKStackPages;
+const unsigned int gR1Pages 	= unsigned int(VMEM_1_SIZE			/ PAGE_SIZE);
+
+// This is the global Kernel page table which is shared by all processes
+struct KernelPageTable
+{
+	PageTableEntry m_pte[gR0Pages];
+};
+
+// The user mode page tables consist of R1 pages and its corresponding stack frames
+struct UserProgPageTable
+{
+	PageTableEntry m_pte[gR1Pages];
+	PageTableEntry m_kstack[gKStackPages];
+};
+
+typedef struct KernelPageTable KernelPageTable;
+typedef struct UserProgPageTable UserProgPageTable;
+
 typedef struct pagetable PageTable;
 
 // this structure is used by the kernel to allocate free frames for the page tables
