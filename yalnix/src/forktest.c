9af640f4fb32d931c6c4f8ef9541eb4bbe7ca0d7
sriravic <srinath.pop@gmail.com>
2017-11-16 14:17:33 -0500

    modified forktest and added gitignore

--- yalnix/src/forktest.c
+++ yalnix/src/forktest.c
@@ -11,34 +11,35 @@ char *argv[];
     if ((pid = Fork()) == 0)
     {
       TracePrintf(0,"CHILD\n");
-      recurse("child", 33);
+      recurse("child", 5, 2);
     }
     else
     {
       TracePrintf(0,"PARENT: child pid = %d\n", pid);
-      recurse("parent", 33);
+      recurse("parent", 5, 1);
     }
 
     Exit(0);
 }
 
-recurse(who, i)
+recurse(who, i, tty_id)
 char *who;
 int i;
+int tty_id;
 {
     char waste[1024];	/* use up stack space in the recursion */
     char *mem = (char *)malloc(2048); /* use up heap space */
     int j;
 
-    for (j = 0; j < 1024; j++) 
+    for (j = 0; j < 1024; j++)
      waste[j] = 'a';
 
-    TtyPrintf(1, "%s %d\n", who, i);
+    TtyPrintf(tty_id, "%s %d\n", who, i);
     if (i == 0)
     {
-	TtyPrintf(1, "Done with recursion\n");
+	TtyPrintf(tty_id, "Done with recursion\n");
 	return;
     }
     else
-	recurse(who, i - 1);
+	recurse(who, i - 1, tty_id);
 }
